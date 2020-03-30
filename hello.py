a5dca56dc13b59acb5fc4aae42a878dd624f3819
MCMC <ztzhou@stanford.edu>
2020-03-29 18:04:53 -0700

    webpage basics

---
+++ hello.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 15:26:14 2020
+
+@author: zitongzhou
+"""
+
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(minutes = 5)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+    # return 'This is the home page <h1>HELLO</h1>'
+
+@app.route('/test')
+def test():
+    return render_template('new.html')
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        flash("Login successful!")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Already logged in!")
+            return redirect(url_for("user"))
+        return render_template("login.html")
+    
+@app.route('/user')
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template('user.html', user = user)
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"{user}, you have been logged out successfully!", "info")
+    
+        session.pop("user", None)
+    else:
+        flash('no login.')
+    return redirect(url_for('login'))
+    
+if __name__ == "__main__":
+    app.run(debug = True)
